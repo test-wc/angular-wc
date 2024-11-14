53f30e4e3c24ba68553973395551dbbfc8de9368
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-11-14 23:32:16 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,17 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
+  currentValue = signal('');
   title = 'angular-wc';
+  handleInput($event: Event) {
+    const inputElement = $event.target as HTMLInputElement;
+    this.currentValue.set(inputElement.value);
+  }
+  handleClick() {
+    alert(this.currentValue());
+  }
 }
